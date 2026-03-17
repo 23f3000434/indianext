c79e08b298ee058dbfad72dc853cd08abadf05ff
23f3000434 <ashuathu93@gmail.com>
2026-03-17 11:05:08 +0530

    Added safewaves

--- frontend/src/components/results/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ResultPanel } from './ResultPanel';
-export { default as ShapVisualization } from './ShapVisualization';
-export { default as ExplanationCard } from './ExplanationCard';
-export { default as RecommendationCard } from './RecommendationCard';
